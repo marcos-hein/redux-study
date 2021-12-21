03e7c43714d286ecb47eaaa9c9a5bdf0bac44a9a
Marcos V. H. Pereira <60493029+marcos-hein@users.noreply.github.com>
2021-12-21 11:49:00 -0300

    Abstracting catalog item

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,10 @@
 import api from '../services/api'
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux";
 import { IProduct } from "../store/modules/cart/types";
-import { addProductToCart } from '../store/modules/cart/actions'
+import CatalogItem from './CatalogItem'
 
 export const Catalog: React.FC = () => {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -14,26 +13,12 @@ export const Catalog: React.FC = () => {
     })
   }, []);
 
-  const handleAddProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }, [dispatch])
-
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <strong>{product.price}</strong> {"  "}
-
-          <button
-            type="button"
-            onClick={() => handleAddProductToCart(product)}  
-          >
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product}/>
       ))}
     </main>
   )
