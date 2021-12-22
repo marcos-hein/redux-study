14564e3a223bf8144095ff1c38793132f0e5b50a
Marcos V. H. Pereira <60493029+marcos-hein@users.noreply.github.com>
2021-12-22 09:36:31 -0300

    Finishing stock check

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -27,6 +27,24 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
 
         break;
       }
+      case 'ADD_PRODUCT_TO_CART_FAILURE': {
+        // const { product } = action.payload;
+
+        // const productInCartIndex = draft.items.findIndex(item =>
+        //   item.product.id === product.id,
+        // );
+
+        // if (productInCartIndex >= 0) {
+        //   draft.items[productInCartIndex].quantity++;
+        // } else {
+        //   draft.items.push({
+        //     product,
+        //     quantity: 1,
+        //   });
+        // }
+
+        break;
+      }
       default: {
         return draft;
       }
